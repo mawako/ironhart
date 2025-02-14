1ebb1698fdc9744d779c03035d9a04bb8d46fe05
Marcel Wa-Ko <marcel.wk@proton.me>
2025-02-14 22:10:02 +0000

    Attempting to add camera system

--- main.lua
+++ main.lua
@@ -2,10 +2,13 @@ require "src/player"
 require "src/assets"
 
 function love.load()
+	camera = require("lib/camera")
 	anim8 = require("lib/anim8")
 	sti = require("lib/sti")
 	
 	love.graphics.setDefaultFilter("nearest", "nearest")
+
+	gameMap = sti("maps/map1.lua")
 
 	load_assets()
 
@@ -34,18 +37,24 @@ function love.update(dt)
 
 end
 
-function love.draw() 
-	love.graphics.draw(assets.background,
-		0, -- x position
-		0, -- y position
-		nil, -- angle, in radians
-		2,2) -- scaling factor (original image scale times by number given) (x, y)
+function love.draw()
+	camera:attach()
 
-	player.anim:draw(player.spritesheet,
-		player.x,
-		player.y,
-		nil,
-		player.scaleX,
-		player.scaleY,
-		16, 16)
+		love.graphics.draw(assets.background,
+			0, -- x position
+			0, -- y position
+			nil, -- angle, in radians
+			2,2) -- scaling factor (original image scale times by number given) (x, y)
+
+		gameMap:draw()
+
+		player.anim:draw(player.spritesheet,
+			player.x,
+			player.y,
+			nil,
+			player.scaleX,
+			player.scaleY,
+			16, 16)
+		
+	camera:detach()
 end
