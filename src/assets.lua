0b7d24e821dd6595a1c1c0d7ea06883da2fc25e0
Marcel Wa-Ko <marcel.wk@proton.me>
2025-02-18 22:34:22 +0000

    Added 'alagard' font to assets.lua

--- src/assets.lua
+++ src/assets.lua
@@ -7,4 +7,5 @@ function load_assets()
 	assets.gfx.spritesheet = love.graphics.newImage("assets/gfx/spritesheet.png")
 
 	assets.font.iosevka = love.graphics.newFont("assets/fonts/iosevka.ttf", 20)
+	assets.font.alagard = love.graphics.newFont("assets/fonts/alagard.ttf", 20)
 end
