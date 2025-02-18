5458acb15c5105509c87940da7b4bf578f8ea0eb
Marcel Wa-Ko <marcel.wk@proton.me>
2025-02-18 23:43:56 +0000

    Changed variables for better readability

--- src/player.lua
+++ src/player.lua
@@ -13,11 +13,11 @@ function player_settings()
 	player.grid = anim8.newGrid(32, 32, player.spritesheet:getWidth(), player.spritesheet:getHeight())
 
 	player.animations = {}
-	player.animations.idle = anim8.newAnimation(player.grid("1-4", 1), 0.3)
-	player.animations.up = anim8.newAnimation(player.grid("1-8", 3), 0.1)
-	player.animations.down = anim8.newAnimation(player.grid("1-8", 5), 0.1)
-	player.animations.left_right = anim8.newAnimation(player.grid("1-8", 2), 0.1)
-	player.animations.attack = anim8.newAnimation(player.grid("1-6", 17), 0.1)
+	player.animations["idle"] = anim8.newAnimation(player.grid("1-4", 1), 0.3)
+	player.animations["up"] = anim8.newAnimation(player.grid("1-8", 3), 0.1)
+	player.animations["down"] = anim8.newAnimation(player.grid("1-8", 5), 0.1)
+	player.animations["left_right"] = anim8.newAnimation(player.grid("1-8", 2), 0.1)
+	player.animations["attack"] = anim8.newAnimation(player.grid("1-6", 17), 0.1)
 
 	player.anim = player.animations.idle
 end
@@ -26,23 +26,23 @@ function player_movement(dt)
 	local velocity_x = 0
 	local velocity_y = 0
 
-	player.anim = player.animations.idle
+	player.anim = player.animations["idle"]
 
 	if love.keyboard.isDown("w") then 
 		velocity_y = (player.speed * dt) * -1
-		player.anim = player.animations.up
+		player.anim = player.animations["up"]
 	elseif love.keyboard.isDown("s") then 
 		velocity_y = (player.speed * dt)
-		player.anim = player.animations.down
+		player.anim = player.animations["down"]
 	end
 
 	if love.keyboard.isDown("a") then 
 		velocity_x = (player.speed * dt) * -1
-		player.anim = player.animations.left_right
+		player.anim = player.animations["left_right"]
 		player.scaleX = -1
 	elseif love.keyboard.isDown("d") then 
 		velocity_x = (player.speed * dt)
-		player.anim = player.animations.left_right
+		player.anim = player.animations["left_right"]
 		player.scaleX = 1
 	end
 
