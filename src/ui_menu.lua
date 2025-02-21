d2c56db7aa79a6ea0da8bd7ca50b9fe0235d9b63
Marcel Wa-Ko <marcel.wk@proton.me>
2025-02-21 19:28:45 +0000

    Added a main menu with a PLAY, HELP and EXIT button

--- src/ui_menu.lua
+++ src/ui_menu.lua
@@ -3,9 +3,27 @@ require("lib/gooi")
 function menu_ui_load()
     
     play = gooi.newButton({
-        text = "Play",
-        x = 20,
-        y = 700,
+        text = "PLAY",
+        x = 1120,
+        y = 339,
+        w = 150,
+        h = 35,
+    }):onRelease(function()
+        gooi.confirm({
+            text = "Begin game?",
+            ok = function()
+                hump_gamestate.switch(game)
+                play.x, play.y = -1000, -1000
+                help.x, help.y = -1000, -1000
+                exit.x, exit.y = -1000, -1000
+            end
+        })
+    end)
+
+    help = gooi.newButton({
+        text = "HELP",
+        x = 1120,
+        y = 384,
         w = 150,
         h = 35,
     }):onRelease(function()
@@ -15,6 +33,21 @@ function menu_ui_load()
         })
     end)
 
+    exit = gooi.newButton({
+        text = "EXIT",
+        x = 1120,
+        y = 429,
+        w = 150,
+        h = 35
+    }):onRelease(function() 
+        gooi.confirm({
+            text = "Are you sure you want to exit?",
+            ok = function() 
+                love.event.quit()
+            end
+        })
+    end)
+    
 end
 
 function menu_ui_update(dt)
