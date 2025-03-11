d0b83298710c04b4fa28425ed7c5c1972abb161a
Marcel Wa-Ko <marcel.wk@proton.me>
2025-03-11 23:09:06 +0000

    Added stamina label

--- src/ui_game.lua
+++ src/ui_game.lua
@@ -5,9 +5,25 @@ function game_ui_load()
     stamina_bar = gooi.newBar({
         value = player.stamina,
         x = 10,
+        y = 45,
+        w = 250,
+        h = 20,
+    })
+
+    stamina_counter = gooi.newLabel({
+        text = "STAMINA",
+        x = 10,
+        y = 45,
+        w = 250,
+        h = 20
+    })
+
+    health_bar = gooi.newBar({
+        value = player.health,
+        x = 10,
         y = 10,
         w = 250,
-        h = 30,
+        h = 20
     })
 
 end
@@ -16,6 +32,9 @@ function game_ui_update(dt)
     
     gooi.update(dt)
     stamina_bar.value = player.stamina
+    health_bar.value = player.health
+
+    stamina_counter.style.fgColor = {19/255, 19/255, 19/255, 1.0}
 
 end
 
