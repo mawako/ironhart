921d9a8166294dc878e56222ae8aa8deeb422c41
Marcel Wa-Ko <marcel.wk@proton.me>
2025-03-11 23:09:33 +0000

    Set border radius to 0

--- lib/gooi/component.lua
+++ lib/gooi/component.lua
@@ -16,11 +16,11 @@ component.colors = {
   darkGrayAlpha = {39/255, 39/255, 39/255, 0.59},
 }
 component.style = {
-  bgColor = component.colors.red,
+  bgColor = component.colors.red, -- Background color
   fgColor = component.colors.white, -- Foreground color
   tooltipFont = love.graphics.newFont(love.window.toPixels(11)), -- tooltips are smaller than the main font
-  radius = 2, -- raw pixels
-  innerRadius = 2, -- raw pixels
+  radius = 0, -- raw pixels
+  innerRadius = 0, -- raw pixels
   showBorder = true, -- border for components
   borderColor = component.colors.blue,
   borderWidth = love.window.toPixels(2), -- in pixels
